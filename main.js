a7141993c60c550775d24e6e4c44ee4942c0693c
Anaëlle RAFELANA <anaelle.rafelana@intra.gobelins.fr>
2026-02-04 17:00:03 +0100

    adding popup map

--- main.js
+++ main.js
@@ -3,26 +3,47 @@ document.addEventListener("DOMContentLoaded", () => {
  let navBar = document.querySelector(".navigation-bar");
 
 navBar.innerHTML = `
-<a id="map-button" class="navigate-link" href="#"> <button class="navigate-btn">
+<a id="map-button" class="navigate-link active-link" href="#map-viewport"> <button class="navigate-btn">
  <img class="nav-picto" src="public/Pictogrammes/boussolePicto.svg" />
  </button>
  </a>
 
- <a id="calendar-button" class="navigate-link" href="#"> <button class="navigate-btn">
+ <a id="calendar-button" class="navigate-link" href="#calendar-viewport"> <button class="navigate-btn">
  <img class="nav-picto" src="public/Pictogrammes/calendarPicto.svg" />
  </button>
  </a>
 
- <a id="chat-button" class="navigate-link" href="#">
+ <a id="chat-button" class="navigate-link" href="#chat-viewport">
  <button class="navigate-btn">
  <img class="nav-picto" src="public/Pictogrammes/messagePicto.svg" />
  </button>
  </a>
 
- <a id="user-button" class="navigate-link" href="#">
+ <a id="user-button" class="navigate-link" href="#user-viewport">
  <button class="navigate-btn">
  <img class="nav-picto" src="public/Pictogrammes/userPicto.svg" />
  </button>
  </a>
  `
+
+ let navButtons = document.querySelectorAll(".navigate-link");
+
+ navButtons.forEach((navBtn) => {
+
+    navBtn.addEventListener("click", () => {
+        
+
+        if(navBtn.classList.contains("active-link")){
+            return
+        }
+        else{
+            
+            navButtons.forEach((btn) => {
+                btn.classList.remove("active-link");
+            })
+        navBtn.classList.add("active-link")
+        }
+    })
+    
+ })
 })
--- scripts/map.js
+++ scripts/map.js
@@ -1,12 +1,59 @@
  document.addEventListener("DOMContentLoaded", () => {
+
+
      mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hZWxsZTEyMyIsImEiOiJjbWw4MndidGEwMjY1M2VxczlrNHRnNGtkIn0.tnkWnrql_viOmVKbEdomvg';
-      const map = new mapboxgl.Map({
+      
+     var monument = [-77.0353, 38.8895];
+     
+     const map = new mapboxgl.Map({
         container: 'boxMap' ,
-        center: [-74, 40.72],
+        center: monument,
         zoom:14,
         style:"mapbox://styles/anaelle123/cml83d51g004q01s9gy9f95ik"
     });
+
+    var popup = new AnimatedPopup(
+        {
+            offset : 25,
+            openingAnimation : {
+                duration :1000,
+                easing : "easeOutElastic",
+                transform : "scale",
+            },
+            closingAnimation : {
+                duration : 300,
+                easing: "easeinBack",
+                transform : "scale"
+            }
+        }
+    ).setText("Visite du washington center");
+
+    let pinElement = document.createElement("div");
+    pinElement.id = "marker";
+
+    new mapboxgl.Marker(pinElement).setLngLat(monument).setPopup(popup).addTo(map)
  })
+
+ function showActivity(){
+
+    let newActivity = document.createElement("div");
+
+    newActivity.className = "activy-popup";
+
+    newActivity.innerHTML = 
+
+    `
+    <div class="activity-event">
+     <img src="public/pictures/washingtonView.webp" />
+     <div class="activity-text">
+     <h1>Visite et promenade</h1>
+     <h2>
+     </div>
+
+     </div>
+    `
+
+ }
  
  
 
