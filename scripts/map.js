a8b450373dd8c171f50deb57ded10145f039b98a
Anaëlle RAFELANA <anaelle.rafelana@intra.gobelins.fr>
2026-02-04 15:22:52 +0100

    fix nav bar

--- scripts/map.js
+++ scripts/map.js
@@ -2,8 +2,9 @@
      mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hZWxsZTEyMyIsImEiOiJjbWw4MndidGEwMjY1M2VxczlrNHRnNGtkIn0.tnkWnrql_viOmVKbEdomvg';
       const map = new mapboxgl.Map({
         container: 'boxMap' ,
-        center: [-74.5, 40], 
+        center: [-74, 40.72],
         zoom:14,
+        style:"mapbox://styles/anaelle123/cml83d51g004q01s9gy9f95ik"
     });
  })
  
