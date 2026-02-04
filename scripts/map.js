8ba817097c0533eb2770d4f194d4ece2decf9a9f
Anaëlle RAFELANA <anaelle.rafelana@intra.gobelins.fr>
2026-02-04 15:07:51 +0100

    adding mapbox

--- scripts/map.js
+++ scripts/map.js
@@ -1,1 +1,11 @@
+ document.addEventListener("DOMContentLoaded", () => {
+     mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hZWxsZTEyMyIsImEiOiJjbWw4MndidGEwMjY1M2VxczlrNHRnNGtkIn0.tnkWnrql_viOmVKbEdomvg';
+      const map = new mapboxgl.Map({
+        container: 'boxMap' ,
+        center: [-74.5, 40], 
+        zoom:14,
+    });
+ })
+ 
+ 
 
