b7a5b5dd2df33cbc37e7062e2132efb6f183b07b
Anaëlle RAFELANA <anaelle.rafelana@intra.gobelins.fr>
2026-02-04 11:44:49 +0100

    init all basic

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://odqoqxdmqsjqdqlkvmzg.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+
--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,1 @@
-mapboxgl.accessToken = "YOUR_MAPBOX_ACCESS_TOKEN";
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [-74.5, 40],
-  zoom: 9,
-});
+
