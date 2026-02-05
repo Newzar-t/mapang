4b7d5d7245d3afc506f1266c7a710ce0a9d9d475
Anaëlle RAFELANA <anaelle.rafelana@intra.gobelins.fr>
2026-02-05 12:17:34 +0100

    finishing map starting calendar

--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,11 @@
  document.addEventListener("DOMContentLoaded", () => {
-
+    
 
      mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hZWxsZTEyMyIsImEiOiJjbWw4MndidGEwMjY1M2VxczlrNHRnNGtkIn0.tnkWnrql_viOmVKbEdomvg';
       
      var monument = [-77.0353, 38.8895];
+     var restaurant = [-77.0321, 38.8975] ;
+     var walking = [ -77.0445, 38.8909 ]
      
      const map = new mapboxgl.Map({
         container: 'boxMap' ,
@@ -12,66 +14,123 @@
         style:"mapbox://styles/anaelle123/cml83d51g004q01s9gy9f95ik"
     });
 
-    var popup = new AnimatedPopup(
-        {
-            offset : 25,
-            openingAnimation : {
-                duration :1000,
-                easing : "easeOutElastic",
-                transform : "scale",
-            },
-            closingAnimation : {
-                duration : 300,
-                easing: "easeinBack",
-                transform : "scale"
-            }
-        }
-    ).setText("Visite du washington center");
 
     let pinElement = document.createElement("div");
-    pinElement.id = "marker";
+    pinElement.id = "markerMuseum";
+
+    let pinRestaurant = document.createElement("div");
+    pinRestaurant.id = "markerResto";
+
+    let pinHiking = document.createElement("div");
+    pinHiking.id = "markerHiking";
 
     let isEventOnScreen = false
 
     pinElement.addEventListener("click", () => {
-        if(isEventOnScreen == false){
-      activityDisplay();
+     let popups = document.querySelectorAll(".activity-container")
+     if(isEventOnScreen == false){
+      popups.forEach((pop) => {
+      pop.style.display = "none"
+      })
+              document.querySelector("#Pedro").style.display = "flex"
       isEventOnScreen = true;
         }
         else{
-            console.log("activity on screen !")
-        }
-        
+            isEventOnScreen = false
+        }  
     })
 
-    new mapboxgl.Marker(pinElement).setLngLat(monument).setPopup(popup).addTo(map);
+    pinRestaurant.addEventListener("click", () => {
+
+     let popups = document.querySelectorAll(".activity-container")
+
+     if(isEventOnScreen == false){
+      popups.forEach((pop) => {
+        pop.style.display = "none"
+      })
+
+      document.querySelector("#Yerin").style.display = "flex"
+
+      isEventOnScreen = true;
+        }
+        else{
+            isEventOnScreen = false
+        }  
+    })
+
+    pinHiking.addEventListener("click", () => {
+
+     let popups = document.querySelectorAll(".activity-container")
+     if(isEventOnScreen == false){
+      popups.forEach((pop) => {
+        pop.style.display = "none"
+      })
+
+      document.querySelector("#Tanaka").style.display = "flex"
+
+      isEventOnScreen = true;
+        }
+        else{
+            isEventOnScreen = false
+        }  
+    })
+
+    new mapboxgl.Marker(pinElement).setLngLat(monument).addTo(map);
+   new mapboxgl.Marker(pinRestaurant).setLngLat(restaurant).addTo(map);
+    new mapboxgl.Marker(pinHiking).setLngLat(walking).addTo(map);
+
+          activityDisplay("public/pictures/restaurantImage.jpg", "Repas gastronomique", "12h-14h", "Yerin");
+                activityDisplay("public/pictures/hikingImage.jpg", "Balade et gaufres !", "16h-19h", "Tanaka");
+                      activityDisplay("public/pictures/washingtonView.webp", "Visite du Washington Center", "15h-17h", "Pedro");
+
+   let closingBtns = document.querySelectorAll(".closingBtn");
+
+
+    closingBtns.forEach((btn) => {
+
+      let popups = document.querySelectorAll(".activity-container")
+      
+      btn.addEventListener("click", () => {
+       
+              popups.forEach((pop) => {
+      pop.style.display = "none"
+      })
+
+      })
+
+   })
 
  })
 
  
-    function activityDisplay(mapPin) {
+    function activityDisplay(urlImage, activityTitle, activityHour, pseudo) {
       let newActivity = document.createElement("div");
 
       newActivity.className = "activity-container";
+      newActivity.id = `${pseudo}`
 
       newActivity.innerHTML = `
-    <div class="activity-event">
 
-     <img class="activity-event-image" src="public/pictures/washingtonView.webp" />
+
+     <img class="activity-event-image" src="${urlImage}" />
+     <button class="closingBtn">
+     <svg width="18px" height="18px" viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cross</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-469.000000, -1041.000000)" fill="#F5F0E4"> <path d="M487.148,1053.48 L492.813,1047.82 C494.376,1046.26 494.376,1043.72 492.813,1042.16 C491.248,1040.59 488.712,1040.59 487.148,1042.16 L481.484,1047.82 L475.82,1042.16 C474.257,1040.59 471.721,1040.59 470.156,1042.16 C468.593,1043.72 468.593,1046.26 470.156,1047.82 L475.82,1053.48 L470.156,1059.15 C468.593,1060.71 468.593,1063.25 470.156,1064.81 C471.721,1066.38 474.257,1066.38 475.82,1064.81 L481.484,1059.15 L487.148,1064.81 C488.712,1066.38 491.248,1066.38 492.813,1064.81 C494.376,1063.25 494.376,1060.71 492.813,1059.15 L487.148,1053.48" id="cross" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>
+     </button>
      <div class="activity-text">
-     <h2>Visite et promenade</h2>
-     <h3>14h-17h</h3>
+     <h2 class="activity-title" >${activityTitle}</h2>
+     <h3>${activityHour}</h3>
 
-     <p>Organisé par Monica</p>
+     <p>Organisé par ${pseudo}</p>
      <span class="activity-footer">
      <button class="basic-button activity-button">Rejoindre <img class="btn-cross-picto" src="public/Pictogrammes/addCross.svg" /></button>
      <img class="activity-group-preview" src="public/pictures/peopleGroupPreview.png" />
      </span>
      </div>
-     </div>
+
 
     `;
 
+    newActivity.style.display = "none"
       document.body.appendChild(newActivity);
     }
 
